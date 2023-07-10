3c8ac133efbc15a754e77f61524da8426a61758a
@Andresmontoya985 <1dandresmontoya@gmail.com>
2023-07-10 15:18:12 +0200

    Primer commit de este repo

---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,16 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
+export const JournalPage = () => {
+    return (
+        <JournalLayout>
+            <Typography variant="p">Et amet adipisicing voluptate commodo id sint consequat exercitation non aliquip commodo irure tempor eu. Magna irure magna esse aute ea et consequat velit sit adipisicing labore eiusmod est ullamco. Eiusmod laboris id reprehenderit cillum. Dolor ad non laborum sunt eu in cillum aute proident quis. Proident tempor exercitation Lorem amet exercitation aute irure proident nisi occaecat duis mollit nostrud.</Typography>
+
+            {/* NothingSelected */}
+            {/* NoteView */}
+
+        </JournalLayout>
+
+    )
+}
