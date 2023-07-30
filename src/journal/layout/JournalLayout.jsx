71be8dacc18d06927f4eee58d9948adea1a076af
@Andresmontoya985 <1dandresmontoya@gmail.com>
2023-07-30 11:50:06 +0200

    Segundo Commit

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,17 +1,17 @@
-import { Box } from "@mui/material"
-import { NavBar } from "../components";
+import { Box, Toolbar } from "@mui/material"
+import { NavBar, SideBar } from "../components";
 
-const drawerWidth = 240;
+const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
     return (
         <Box sx={{ display: 'flex' }}>
             <NavBar drawerWidth={drawerWidth} />
 
-            {/* SideBar drawerWidth */}
+            <SideBar drawerWidth={drawerWidth} />
             <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
 
-                {/* Toolbar */}
+                <Toolbar />
 
                 {children}
             </Box>
